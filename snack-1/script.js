07321428e8ebab3462292bf9b87c3ee5b418e2f3
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-21 14:36:51 +0100

    [fix ex-01]

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,10 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
-console.log(names);
+
+names.forEach((Element) => {
+    console.log(`
+       ${names} `)
+       
+})
+
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
