a360de1cbe5c08bf32dfe8095cdef1e43fa3c3d9
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-21 16:15:40 +0100

    [ex-05]

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,10 @@
 const nums = [2, 8, 4, 7, 12, 87];
-
+const pari = nums.filter((nums) => {
+    if(nums % 2 === 0) {
+        return true;
+    }
+    return false;
+});
+console.log(pari);
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
