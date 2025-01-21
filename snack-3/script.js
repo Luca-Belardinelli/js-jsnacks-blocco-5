d28873a18b6dca26ff9aa7519fa19308079c951b
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-21 16:12:01 +0100

    [ex-04]

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,7 +1,7 @@
 const numbers = [2, 8, 4, 7, 2, 87];
-const incremento = numbers.map((numero) => {
+const increase = numbers.map((numero) => {
     return numero + 1;
 });
-console.log(incremento);
+console.log(increase);
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
--- snack-4/script.js
+++ snack-4/script.js
@@ -4,6 +4,9 @@ const posts = [
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
-
+const newspaper = posts.map((posts) => {
+  return  posts.author;
+});
+console.log(newspaper);
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
