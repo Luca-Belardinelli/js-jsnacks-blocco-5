03a66280e7580b8e3dd7e2912a334c6bb41c11d3
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-21 16:26:31 +0100

    [ex-07]

--- snack-7/script.js
+++ snack-7/script.js
@@ -19,5 +19,10 @@ const students = [
   },
 ];
 
+const myPerson = students.find((person) => {
+  return person.id === 2;
+});
+console.log(myPerson);
+
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
